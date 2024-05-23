2f8f9c66c01e9e270bca1c0b858be1de7e03eab5
Valentinaalejandrajara <valentinaalejandra.jara@alumnos.ulagos.cl>
2024-05-22 22:27:28 -0400

    Update Ejercicio 2 tarea 5.py

--- Ejercicio 2 tarea 5.py
+++ Ejercicio 2 tarea 5.py
@@ -3,10 +3,13 @@
 n = (input("Ingrese el primer nombre de pila: "))
 n1 = (input("Ingrese el segundo nombre de pila: "))
 
+#Se crea una lista que contiene los nombres
 nombres = [n, n1]
+#El comando sort () se utiliza para ordenar los elementos de una lista en orden ascendente
 nombres.sort()
 
-
+#Se ocupa el bucle for para itinerar cada elemento de la lista anterior "nombres"
 print("Los nombres ordenados alfabeticamente: ")
 for nombre in nombres:
     print(nombre)
+    #Cada itineracion, imprime el nombre actual 
